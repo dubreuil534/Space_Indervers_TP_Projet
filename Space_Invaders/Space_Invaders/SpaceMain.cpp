fe1535e9f82b41eb2b08e19651d32520b4599afc
JC <jcturcot@gmail.com>
2016-05-03 14:42:33 -0400

    variable "monVaisseauX" dans main + collisions

--- Space_Invaders/Space_Invaders/SpaceMain.cpp
+++ Space_Invaders/Space_Invaders/SpaceMain.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 #include "Coord.h"
@@ -7,12 +8,26 @@ using namespace std;
 #include "Martien.h"
 #include "Vaisseau.h"
 
-int main()
-{
+int main(){
 
 	Vaisseau monVaisseau;
+	int monVaisseauX = 20;
 
+	while(true){
+		char touche = _getch();
 
+		if (touche == 'l' || touche == 'k'){
+			if (touche == 'l' && monVaisseauX < 40){
+				monVaisseauX++;
+				monVaisseau.modifierPosition(touche);
+			}
+			else if (touche == 'k' && monVaisseauX > 0){
+				monVaisseauX--;
+				monVaisseau.modifierPosition(touche);
+			}
+		}
+	}
+	
 
 
 	return 0;
